bb05d378ca0a6dc1de40bb76a0ba6878c5c8da5c
Jan Voracek <jan@voracek.net>
2023-12-09 08:56:39 +0100

    Day 9

---
+++ 09/solution-part2.ts
@@ -0,0 +1,19 @@
+const extrapolate = (values: number[]): number => {
+  if (values.every(v => v === 0)) return 0;
+
+  const deltas = values.map((num, i) => num - values[i - 1]).slice(1);
+  return values[values.length - 1] + extrapolate(deltas);
+};
+
+function solve(input: string) {
+  return input
+    .trim()
+    .split('\n')
+    .map(line => line.split(' ').map(Number).toReversed())
+    .map(extrapolate)
+    .reduce((acc, cur) => acc + cur, 0);
+}
+
+const input = (await Bun.file('input.txt').text()).trim();
+const result = solve(input);
+console.log(result);
