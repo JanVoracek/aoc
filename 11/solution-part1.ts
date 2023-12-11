fdc73350d3bdb0097f6776830147d0b977890093
Jan Voracek <jan@voracek.net>
2023-12-11 09:11:21 +0100

    Day 11 – refactoring

--- 11/solution-part1.ts
+++ 11/solution-part1.ts
@@ -1,26 +1,23 @@
-function column(lines: string[], index: number): string {
-  return lines.map(line => line[index]).join('');
-}
+type Sliceable<T> = ArrayLike<T> & { slice: Array<T>['slice'] };
 
 function expandUniverse(input: string) {
   const lines = input.split('\n');
-  const universe: number[][] = lines
-    .map(line => line.split('').map(c => 1));
 
-  for (let i = 0; i < universe.length; i++) {
-    const line = lines[i];
-    const empty = !line.includes('#');
-    universe[i] = universe[i].map(c => (empty ? 2 : c));
-  }
+  const emptyLines = new Set(
+    lines
+      .map((line, i) => [line, i] as const)
+      .filter(([line]) => !line.includes('#'))
+      .map(([_, i]) => i)
+  );
 
-  for (let i = 0; i < universe[0].length; i++) {
-    const col = column(lines, i);
-    const empty = !col.includes('#');
-    for (let j = 0; j < universe.length; j++) {
-      universe[j][i] = empty ? 2 : universe[j][i];
-    }
-  }
-  return universe;
+  const emptyColumns = new Set(
+    Array.from({ length: lines[0].length })
+      .map((_, i) => [columnSlice(lines, i, 0), i] as const)
+      .filter(([column]) => !column.includes('#'))
+      .map(([_, i]) => i)
+  );
+
+  return lines.map((line, x) => line.split('').map((_, y) => (emptyLines.has(x) || emptyColumns.has(y) ? 2 : 1)));
 }
 
 function findGalaxies(input: string): [number, number][] {
@@ -28,29 +25,23 @@ function findGalaxies(input: string): [number, number][] {
   return [...input.matchAll(/(#)/g)].map(m => [Math.floor(m.index! / universeWidth), m.index! % universeWidth]);
 }
 
-function getPairs<T>(input: T[]): [T,T][] {
-  const combinations: [T,T][] = [];
-  for (let i = 0; i < input.length; i++) {
-    for (let j = i + 1; j < input.length; j++) {
-      combinations.push([input[i], input[j]]);
-    }
-  }
-  return combinations;
+function getPairs<T>(input: T[]): [T, T][] {
+  return input.flatMap((a, i) => input.slice(i + 1).map(b => [a, b] as [T, T]));
+}
+
+function columnSlice<T>(matrix: Sliceable<ArrayLike<T>>, column: number, start: number, end?: number): T[] {
+  return matrix.slice(start, end).map(row => row[column]);
+}
+
+function sum(input: number[]): number {
+  return input.reduce((a, b) => a + b, 0);
 }
 
 function getDistance(universe: number[][], a: [number, number], b: [number, number]) {
-  const minX = Math.min(a[0], b[0]);
-  const maxX = Math.max(a[0], b[0]);
-  const minY = Math.min(a[1], b[1]);
-  const maxY = Math.max(a[1], b[1]);
-  let distance = 0;
-  for (let x = minX + 1; x <= maxX; x++) {
-    distance += universe[x][minY];
-  }
-  for (let y = minY + 1; y <= maxY; y++) {
-    distance += universe[maxX][y];
-  }
-  return distance;
+  const [x1, x2] = [Math.min(a[0], b[0]) + 1, Math.max(a[0], b[0])];
+  const [y1, y2] = [Math.min(a[1], b[1]) + 1, Math.max(a[1], b[1])];
+
+  return sum(columnSlice(universe, y2, x1, x2 + 1)) + sum(universe[x2].slice(y1, y2 + 1));
 }
 
 const input = (await Bun.file('input.txt').text()).trim();
@@ -62,4 +53,3 @@ const pairs = getPairs(galaxies);
 
 const result = pairs.map(([a, b]) => getDistance(universe, a, b));
 console.log(result.reduce((a, b) => a + b, 0));
-
