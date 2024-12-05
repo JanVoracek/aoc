1ad3c6d5add03af822599b7473eb3af829ecea9d
Jan Voracek <jan@voracek.net>
2024-12-05 08:58:09 +0100

    Day 5 - simplify

--- 2024/05/main.ts
+++ 2024/05/main.ts
@@ -7,53 +7,42 @@ export function parse(input: string) {
   const [rawOrderings, rawUpdatePages] = input.trim().split('\n\n');
 
   return {
-    orderings: rawOrderings.split('\n').map(line => line.split('|').map(Number) as [number, number]),
-    updates: rawUpdatePages.split('\n').map(line => line.split(',').map(Number)),
+    orderings: rawOrderings.split('\n'),
+    updates: rawUpdatePages.split('\n').map(line => line.split(',')),
   };
 }
 
 export function solvePart1(input: ParseOutput) {
   return input.updates
     .filter(pages => isOrdered(input.orderings, pages))
-    .map(pages => middle(pages))
+    .map(pages => Number(middle(pages)))
     .reduce(sum);
 }
 
 export function solvePart2(input: ParseOutput) {
   return input.updates
     .filter(pages => !isOrdered(input.orderings, pages))
-    .map(pages => middle(reOrder(input.orderings, pages)))
+    .map(pages => Number(middle(reOrder(input.orderings, pages))))
     .reduce(sum);
 }
 
-function isOrdered(orderings: [number, number][], pages: number[]): boolean {
-  return orderings.every(([x, y]) => {
-    return orderInfo(pages, x, y).ordered;
-  });
+function isOrdered(orderings: string[], pages: string[]): boolean {
+  return orderings.every(ordering => arePagesOrdered(pages, ordering));
 }
 
-type OrderInfo = {
-  ordered: boolean;
-  xi: number;
-  yi: number;
-};
-
-function orderInfo(pages: number[], x: number, y: number): OrderInfo {
+function arePagesOrdered(pages: string[], ordering: string): boolean {
+  const [x, y] = ordering.split('|');
   const xi = pages.indexOf(x);
   const yi = pages.indexOf(y);
-  return { ordered: xi === -1 || yi === -1 || xi < yi, xi, yi };
+  return xi === -1 || yi === -1 || xi < yi;
 }
 
-function reOrder(orderings: [number, number][], pages: number[]): number[] {
-  while (!isOrdered(orderings, pages)) {
-    for (const [x, y] of orderings) {
-      const { ordered, xi, yi } = orderInfo(pages, x, y);
-      if (!ordered) {
-        [pages[xi], pages[yi]] = [pages[yi], pages[xi]];
-      }
-    }
-  }
-  return pages;
+function reOrder(orderings: string[], pages: string[]): string[] {
+  return pages.slice().sort((a, b) => {
+    if (orderings.includes(`${a}|${b}`)) return -1;
+    if (orderings.includes(`${b}|${a}`)) return 1;
+    return 0;
+  });
 }
 
 function middle<T>(array: T[]) {
