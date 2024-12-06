b9a52927ce10332e44a5fd03b55572c1f8dc2843
Jan Voracek <jan@voracek.net>
2024-12-06 08:01:41 +0100

    Add more useful utils

--- 2024/shared/math.ts
+++ 2024/shared/math.ts
@@ -1,7 +1,15 @@
 export function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 export function product(a: number, b: number) {
-    return a * b;
+  return a * b;
+}
+
+export function range(start: number, end: number) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+}
+
+export function cartesianProduct<T>(a: T[], b: T[]) {
+  return a.flatMap(x => b.map(y => [x, y] as [T, T]));
 }
--- 2024/shared/runtime.ts
+++ 2024/shared/runtime.ts
@@ -5,8 +5,10 @@ export async function run<T>(parse: (input: string) => T, part1: (input: T) => u
   const currentInput = inputType === 'example' ? await readExample() : await readInput();
   const solveFunction = partNumber === '1' ? part1 : part2;
 
+  console.time('Execution time');
   const parsed = parse(currentInput);
   console.log(solveFunction(parsed));
+  console.timeEnd('Execution time');
 }
 
 export async function readExample() {
@@ -15,4 +17,17 @@ export async function readExample() {
 
 export async function readInput() {
   return await Deno.readTextFile('input.txt');
+}
+
+export function memoize<TArgs extends unknown[], TReturn>(fn: (...args: TArgs) => TReturn) {
+  const cache = new Map<string, TReturn>();
+  return (...args: TArgs) => {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key)!;
+    }
+    const result = fn(...args);
+    cache.set(key, result);
+    return result;
+  };
 }
